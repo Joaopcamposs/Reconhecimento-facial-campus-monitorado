76f33ef2fa3d93e14db5856536e5fff734949dba
Joao Pedro Campos <joaopcamposs@outlook.com>
2021-07-06 14:54:45 -0300

    Modificacao no arquivo captura, adicionando funcao para salvar faces para treinamentos posteriormente

--- captura.py
+++ captura.py
@@ -1,8 +1,14 @@
 import cv2
 
 classificador = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-cameraIP = cv2.VideoCapture(0) #WebCam
-#cameraIP = cv2.VideoCapture('rtsp://joaop:Jp103266@192.168.0.107/') #cameraIP
+#cameraIP = cv2.VideoCapture(0) #WebCam
+cameraIP = cv2.VideoCapture('rtsp://joaop:Jp103266@192.168.0.107/') #cameraIP
+
+amostra = 1
+numeroAmostras = 25
+id = input('Digite seu identificador: ')
+largura, altura = 220, 220
+print("Capturando as faces...")
 
 while(True):
     conectado, imagem = cameraIP.read()
@@ -10,11 +16,19 @@ while(True):
     imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
     facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(150, 150))
 
-    for(x, y, l, a) in facesDetectadas:
-        cv2.rectangle(imagem, (x, y), (x+l, y+a), (0, 0, 255), 2)
+    for (x, y, l, a) in facesDetectadas:
+        cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 2)
+        if cv2.waitKey(1) & 0xFF == ord('q'): #tecla 'q' captura as fotos
+            imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+            cv2.imwrite("fotos/pessoa." + str(id) + "." + str(amostra) + ".jpg", imagemFace)
+            print("[Foto" + str(amostra) + " capturada com sucesso]")
+            amostra += 1
 
     cv2.imshow("Face", imagem)
-    cv2.waitKey(1)
+    #cv2.waitKey(1)
+    if (amostra >= numeroAmostras + 1):
+        break
 
+print("Faces capturadas com sucesso!")
 cameraIP.release()
 cv2.destroyAllWindows()
